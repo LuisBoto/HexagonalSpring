eed01e2bf4407cb2822e23b69e5c2c3d26279ac9
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2023-10-19 17:13:25 +0200

    chore: Added module structure

---
+++ inbound/src/main/java/org/inbound/ApiController.java
@@ -0,0 +1,10 @@
+package org.inbound;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+
+
+}
---
+++ inbound/src/test/java/org/inbound/ApiControllerTest.java
@@ -0,0 +1,14 @@
+package org.inbound;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class ApiControllerTest {
+
+	@Test
+	public void testHello() throws Exception {
+		fail("No test implemented.");
+	}
+
+}
