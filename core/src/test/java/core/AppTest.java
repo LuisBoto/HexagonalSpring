284dcd59099293a2294203dc3b9ca36d4b99ec5c
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2023-10-19 17:30:38 +0200

    chore: cleanup and startup application

--- core/src/test/java/core/AppTest.java
+++ core/src/test/java/core/AppTest.java
@@ -3,15 +3,11 @@ package core;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.system.CapturedOutput;
-import org.springframework.boot.test.system.OutputCaptureExtension;
 
-@ExtendWith(OutputCaptureExtension.class)
 public class AppTest {
 
     @Test
-    void testHello(CapturedOutput capture) throws Exception {
+    void testHello() throws Exception {
 		fail("No test implemented.");
     }
 }
