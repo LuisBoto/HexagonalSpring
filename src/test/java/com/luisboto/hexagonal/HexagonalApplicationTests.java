37ca4e3982e3ecff4ee569824a8bef68a3cef9c7
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2023-10-19 16:35:21 +0200

    chore: Initial codebase from spring initializr

---
+++ src/test/java/com/luisboto/hexagonal/HexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisboto.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
