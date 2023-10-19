27f6d1ca11212ecd61cdbbaf8b8d1f0065fdd927
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2023-10-19 17:59:32 +0200

    fix: spring parent dependencies

---
+++ core/src/main/java/com/luisboto/core/service/TariffService.java
@@ -0,0 +1,12 @@
+package com.luisboto.core.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TariffService {
+	
+	public void getActiveTariff() {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ inbound/src/main/java/com/luisboto/inbound/controller/TariffController.java
@@ -0,0 +1,25 @@
+package com.luisboto.inbound.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luisboto.core.service.TariffService;
+
+@RestController()
+@RequestMapping("/api/v1")
+public class TariffController {
+	
+	private TariffService tariffService;
+	
+	public TariffController(TariffService tariffService) {
+		this.tariffService = tariffService;
+	}
+
+	@GetMapping("/tariff")
+	public void getActiveTariff() {
+		tariffService.getActiveTariff();
+	}
+
+
+}
